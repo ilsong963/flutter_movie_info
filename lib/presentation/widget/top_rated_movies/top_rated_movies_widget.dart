563f896f1c1a108c21a8470ec20cf1a843111471
ilsong963 <ilsong963@naver.com>
2025-05-14 19:17:34 +0900

    feat: TopRatedMoviesViewModel 작성

---
+++ lib/presentation/widget/top_rated_movies/top_rated_movies_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movie_info/presentation/widget/common/poster_widget.dart';
+import 'package:flutter_movie_info/presentation/widget/top_rated_movies/top_rated_movies_view_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TopRatedMoviesWidget extends ConsumerWidget {
+  const TopRatedMoviesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final movies = ref.watch(topRatedMoviesViewModel);
+    if (movies == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: movies.length,
+      itemBuilder: (context, index) {
+        final movie = movies[index];
+
+        return PosterWidget(posterPath: movie.posterPath);
+      },
+    );
+  }
+}
