80d688da75f73a485c9009f8c1a81d8d7b7ed015
ilsong963 <ilsong963@naver.com>
2025-05-14 11:19:50 +0900

    test: 테스트를 위한 ==, hashcode오버라이딩

--- lib/domain/entity/movie.dart
+++ lib/domain/entity/movie.dart
@@ -3,4 +3,15 @@ class Movie {
   final String posterPath;
 
   Movie({required this.id, required this.posterPath});
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (other is! Movie) return false;
+
+    return other.id == id;
+  }
+
+  @override
+  int get hashCode => id.hashCode;
 }
